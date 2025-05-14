3e4a79e5aea2ce621bfa267ec78acbde128197ea
urmilpandey <urmil.pandey@gmail.com>
2025-05-15 03:26:25 +0530

    Commit 2

---
+++ Task_A3_2.py
@@ -0,0 +1,9 @@
+#module
+#from math import sqrt,log,sin
+from math import *
+
+n = int(input("Enter a number: "))
+print(f" Square root of {n} is {sqrt(n)}")
+print(f" Natural logarithm (log base e) of the number is {log(n,e)}")
+print(f" Sine of the number is {sin(n)}")
+
