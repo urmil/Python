c43e95c3c22d47dd4c4e9420ca7eee4b987e6005
urmilpandey <urmil.pandey@gmail.com>
2025-05-15 11:13:00 +0530

    Commit 2

--- Assignment4/Task_A4_1.py
+++ Assignment4/Task_A4_1.py
@@ -1,11 +1,15 @@
 
 def reading(file):
     with open(file,'r+') as f:
-        print(f.read())
+       print("Reading file Content")
+       num =0
+       for line in f:
+         num += 1
+         print(f"Line {num}: {line}")
 
 
 def main():
-    reading(r'../Sample.txt')
+    reading(r'./Sample.txt')
 
 if __name__ == '__main__':
     main()
